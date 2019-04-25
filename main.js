7ff94692ee821e9a89d3ee271f39c0a4030cdaf3
Mannex Pimentel <mannex247@gmail.com>
2019-04-25 17:53:08 -0400

    jQuery plugin

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.js--projects').waypoint(function(direction){
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+        $('nav').removeClass('sticky');
+        }
+    })
+
+   
+})
